702e7f81c635d6cc76cdfb0320d1f272f4934154
thesmokemachine <elsmokemachine@gmail.com>
2013-06-26 20:58:24 -0400

    mapreducey.py: more proper work routing, work doing flow.  fabulous new image_resize work function defined.

--- mapreducey.py
+++ mapreducey.py
@@ -7,8 +7,13 @@ import requests
 app = Flask(__name__)
 
 def start_app(**kwargs):
+    global work
+    work = ['image_resize']
+
     global workers
     workers = ['mapreducey-1.appspot.com', 'mapreducey-1.herokuapp.com']
+    #workers = ['localhost:8080','localhost:5000']
+
     global purpose
     if 'purpose' in kwargs and kwargs['purpose'] in ['router','worker']:
         purpose = kwargs['purpose']
@@ -20,16 +25,29 @@ def start_app(**kwargs):
 @app.route('/work')
 def work():
     import random
-    data = {"purpose" : purpose, "message" : "I do work for these params", "params" : request.args}
-    worker = random.choice(workers)
-    callback_url = 'http://' + worker + '/callback/work'
-    r = requests.get(callback_url, params = request.args)
-    data['callback_response'] = r.json()
-    data['worker_url'] = worker
-    data['router_url'] = request.url_root[7:-1]
-    response = jsonify(data)
-    response.status_code = 200
-    return response
+    data = {}
+    params = request.args.to_dict()
+    if not (params['work'] and params['work'] in work):
+        response = {"message": "Work type %s not found." % params['work'], "viable_work" : work}
+        response = jsonify(response)
+        response.status_code = 501
+        return response
+    elif purpose == 'worker' or 'routed' in params.keys():
+        data['response'] = globals()[params['work']](**params)
+        response = jsonify(data)
+        response.status_code = 200
+        return response        
+    elif purpose == 'router':
+        worker = random.choice(workers)
+        callback_url = 'http://' + worker + '/work'
+        params['routed'] = True
+        r = requests.get(callback_url, params = params)
+        data['response'] = r.json()
+        data['worker_url'] = worker
+        data['router_url'] = request.url_root[7:-1]
+        response = jsonify(data)
+        response.status_code = 200
+        return response
 
 @app.route('/callback/work')
 def callbackWork():
@@ -46,6 +64,13 @@ def callbackHereiam():
     return response
 
 
+def image_resize(**kwargs):
+    vars = ['img_url', 'width', 'height']
+    intersection = set(kwargs.keys()) & set(vars)
+    if len(vars) != len(intersection):
+        return "Please pass these vars: %s. I received: %s" % (sorted(vars), sorted(list(intersection)))
+
+    return "<img src='%s' width='%s' height='%s'>" % (kwargs['img_url'], kwargs['width'], kwargs['height'])
 
 
 
